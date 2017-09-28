3cbdc596543fbe4cc4ef857c98eb12efb4e60c9b
kramduckner <buckner.a.mark@gmail.com>
2017-09-28 08:26:28 -0600

    removed ejs dep and added mongoose to server.js

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var express = require("express");
 var app = express();
+var mongoose = require('mongoose');
+
+//mongoose.connect('mongodb://localhost/{put your database right here}');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+    console.log('we have connected');
+});
 
 app.get("/", function(req, res) {
     res.sendfile('index.html');
