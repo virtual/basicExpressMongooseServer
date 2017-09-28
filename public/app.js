830d518a60326f2d34151c01f83df62116e28d01
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-09-28 16:54:53 -0600

    deleted all the poor chairs

--- public/app.js
+++ public/app.js
@@ -1,6 +1,28 @@
-$(function(){
-  $.get('/chairs', function(chairList) {
+$(function () {
+  $.get('/chairs', function (chairList) {
     //console.log(chairList);
     $('#chairTable').append(formatAsTable(chairList));
+
+    $('.delete').on("click", function () {
+      var deleteId = $(this).children(".hideid").text();
+      console.log(deleteId);
+
+      $.ajax({
+        type: 'DELETE',
+        url: '/chairs',
+        data: "id=" + deleteId,
+        success: function(data) {
+          console.log(data);
+          // use jquery to delete the table row only
+          //$("#chairTable").empty();
+          //$('#chairTable').append(formatAsTable(chairList));
+        }
+      });
+
+      // '/chairs/', deleteId, function(chair){
+      //  console.log("it worked", chair);
+      // })
+    });
+
   });
 });
--- public/functions.js
+++ public/functions.js
@@ -3,8 +3,8 @@ var formatAsTable = function (allItems) {
   //console.log(allNames); 
   var itemRow = "";
   allItems.forEach((item) => {
-    itemRow += "<tr>" + '<td><i class="fa fa-trash" aria-hidden="true"></i>' + "</td><td>" + item.type + "</td><td>" + item.model +  "</td></tr>";
+    console.log(item);
+    itemRow += "<tr>" + '<td><button class="delete"><i class="fa fa-trash" aria-hidden="true"></i> Delete <span class="hideid">' + item._id + "</span></button>" + "</td><td>" + item.type + "</td><td>" + item.model +  "</td></tr>";
   }); 
   return itemRow;
 };
- 
